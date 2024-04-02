f3a67c8cf0a107f946787f6d5709542ee794f1eb
Killu <60795569+JustKillu@users.noreply.github.com>
2024-04-02 01:46:08 -0400

    Vieja logica

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -37,10 +37,7 @@ const manifestForPlugin = {
 
 export default defineConfig({
   server: {
-    https: {
-      key: fs.readFileSync('./localhost-key.pem'),
-      cert: fs.readFileSync('./localhost.pem'),
-    },
+   
   },
   plugins: [
     react(),
