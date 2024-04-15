6291128511db7f93a4592d39adbbf72f9ddb78c5
Killu <60795569+JustKillu@users.noreply.github.com>
2024-04-15 00:46:46 -0400

    Updated

--- frontend/src/componentes/Notificaction.jsx
+++ frontend/src/componentes/Notificaction.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
+  import React from 'react';
 
-const Notification = ({ title, description, color }) => {
-  return (
-    <div className={`alert ${color} p-3 rounded`}>
-      <h4 className="font-bold">{title}</h4>
-      <p>{description}</p>
-    </div>
-  );
-};
+  const Notification = ({ title, description, color }) => {
+    return (
+      <div className={`alert ${color} p-3 rounded`}>
+        <h4 className="font-bold">{title}</h4>
+        <p>{description}</p>
+      </div>
+    );
+  };
 
-export default Notification;
+  export default Notification;
